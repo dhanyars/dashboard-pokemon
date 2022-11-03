d12f9f320bd27673efb9aed461e0b13d0daf4f0a
Dhanya R.S <dhanyaswin@gmail.com>
2022-11-03 21:59:23 +0100

    feat: add actions

---
+++ src/app/redux/actions/pokemon.tsx
@@ -0,0 +1,20 @@
+import { action, fsa, payload } from "ts-action";
+
+export const load = action("POKEMON/LOAD", payload());
+
+export interface PokemonData {
+  country_id: number;
+  country_name: string;
+  total_reviews: number;
+  average_review_score: number;
+}
+
+export const updatePokemon = action("POKEMON/UPDATE_POKEMON", fsa<any>());
+export const pokemonDetailsLoad = action(
+  "POKEMON/POKEMON_DETAILS_LOAD",
+  payload()
+);
+export const updatePokemonDetails = action(
+  "POKEMON/UPDATE_POKEMON_DETAILS",
+  fsa<any>()
+);
