97e5cd60aa90c11f30bc00412da132d5de36a1e9
Dhanya R.S <dhanyaswin@gmail.com>
2022-11-03 22:03:08 +0100

    feat: add constants

---
+++ src/app/constants.tsx
@@ -0,0 +1,13 @@
+export const OPTIONS = [
+  { value: "", text: "Sort by" },
+  { value: "name", text: "name" },
+  { value: "weight", text: "weight" },
+  { value: "height", text: "height" },
+];
+
+export const OPTIONS_Records=[
+    { value: "", text: "Records per page" },
+    { value: 20, text: "20" },
+    { value: 30, text: "30" },
+    { value: 50, text: "50" },
+  ];
