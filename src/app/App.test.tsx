ea051d4a7b60258f70a5986818c87f498c5b12d9
Dhanya R.S <dhanyaswin@gmail.com>
2022-11-03 22:04:15 +0100

    feat: add route and app changes

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import * as reducers from "../app/redux/reducers";
+import { combineReducers } from "redux";
+import createSagaMiddleware from "redux-saga";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+const rootReducer = combineReducers(reducers);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [sagaMiddleware],
+});
+
+test("renders learn react link", () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const linkElement = screen.getByText(/Sort by/i);
   expect(linkElement).toBeInTheDocument();
 });
