9ea032604b848facc13c583c63a4d511b251df16
Dhanya R.S <dhanyaswin@gmail.com>
2022-11-04 11:28:16 +0100

    feat: action triggered for filter updates

--- src/app/routes/pokemons/index.tsx
+++ src/app/routes/pokemons/index.tsx
@@ -7,12 +7,13 @@ import { useDispatch } from "react-redux";
 
 const mapStateToProps = (state: AppState) => ({
   pokemon: state.pokemon.pokemonList.data,
+  filters: state.pokemon.filter,
 });
 
 const mapDispatchToProps = (dispatch: any) => ({
   useLoadPage: (url: any) => {
     React.useEffect(() => {
-      dispatch(actions.pokemon.load({url}));
+      dispatch(actions.pokemon.load({ url }));
     }, []);
   },
 });
--- src/app/routes/pokemons/pokemon.tsx
+++ src/app/routes/pokemons/pokemon.tsx
@@ -8,22 +8,30 @@ import Select from "../../components/baseComponents/Select";
 import Input from "../../components/baseComponents/Input";
 import PokemonCard from "../../components/featureComponents/PokemonCard";
 import { OPTIONS, OPTIONS_Records } from "../../constants";
+import "./_pokemon.scss";
+import { useDispatch } from "react-redux";
+import * as actions from "../../redux/actions";
 
-const PokemonDetails: React.FC<PokemonPageProps> = ({ pokemon, useLoadPage }) => {
+const PokemonDetails: React.FC<PokemonPageProps> = ({
+  pokemon,
+  useLoadPage,
+  filters,
+}) => {
   const url = `https://pokeapi.co/api/v2/pokemon?limit=100&offset=0`;
   const [pokemonData, setPokemonData] = useState([]);
   const [filteredData, setFilteredData] = useState(pokemon?.pokemonData);
-  const [search, setSearch] = useState("");
-  const [sort, setSort] = useState("name");
-  const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [search, setSearch] = useState(filters?.search);
+  const [sort, setSort] = useState(filters?.sort);
+  const [currentPage, setCurrentPage] = useState(filters?.currentPage);
+  const [itemsPerPage, setItemsPerPage] = useState(filters?.itemsPerPage);
+  const dispatch = useDispatch();
 
   useLoadPage(url);
 
-  const handleNext = (value: string) => {
+  const handleNext = () => {
     setCurrentPage(currentPage + 1);
   };
-  const handlePrev = (value: string) => {
+  const handlePrev = () => {
     setCurrentPage(currentPage - 1);
   };
 
@@ -47,7 +55,22 @@ const PokemonDetails: React.FC<PokemonPageProps> = ({ pokemon, useLoadPage }) =>
         : -1
     );
     setFilteredData(sorted);
-  }, [pokemonData, search, sort, pokemon?.pokemonData]);
+    dispatch(
+      actions.pokemon.updateFilters({
+        sort,
+        itemsPerPage,
+        currentPage,
+        search,
+      })
+    );
+  }, [
+    pokemonData,
+    search,
+    sort,
+    pokemon?.pokemonData,
+    itemsPerPage,
+    currentPage,
+  ]);
 
   const totalPages = Math.ceil(filteredData?.length / itemsPerPage);
   const indexOfLastCard = currentPage * itemsPerPage;
@@ -57,38 +80,74 @@ const PokemonDetails: React.FC<PokemonPageProps> = ({ pokemon, useLoadPage }) =>
     <Container>
       <Row>
         <div>
-          <Input value={search} onChange={(e) => setSearch(e.target.value)} />
+          <h1>Pokemon Dashboard</h1>
+          <Row className="pokemon-search">
+            <Col md={6}>
+              <div>
+                {" "}
+                <h2>Search pokemon</h2>
+              </div>
 
-          <Select
-            label="Sort by"
-            options={OPTIONS}
-            value={sort}
-            onChange={(e: any) => setSort(e.target.value)}
-          ></Select>
-
-          <Select
-            label="Records per Page"
-            options={OPTIONS_Records}
-            value={itemsPerPage}
-            onChange={(e: any) => setItemsPerPage(e.target.value)}
-          ></Select>
+              <Input
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="Search pokemon by name or abilities"
+              />
+            </Col>
+          </Row>
+          <Row className="pokemon-filter-outer">
+            <Col md={4} sm={12} xs={12}>
+              {filteredData && (
+                <Pagination
+                  onLeftClick={handlePrev}
+                  onRightClick={handleNext}
+                  currentPage={currentPage}
+                  totalPages={totalPages}
+                />
+              )}
+            </Col>
+            <Col md={8} sm={12} xs={12}>
+              <Row className="pokemon-filter">
+                <Col md={4}>
+                  <Select
+                    label="Sort by"
+                    options={OPTIONS}
+                    value={sort}
+                    onChange={(e: any) => setSort(e.target.value)}
+                  ></Select>
+                </Col>
+                <Col md={4}>
+                  <Select
+                    label="Pokemons per Page"
+                    options={OPTIONS_Records}
+                    value={itemsPerPage}
+                    onChange={(e: any) => setItemsPerPage(e.target.value)}
+                  ></Select>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
 
           {filteredData && (
-            <div>
-              <Pagination
-                onLeftClick={handlePrev}
-                onRightClick={handleNext}
-                currentPage={currentPage}
-                totalPages={totalPages}
-              />
-
-              <PokemonCard
-                data={filteredData}
-                indexOfFirstCard={indexOfFirstCard}
-                indexOfLastCard={indexOfLastCard}
-              />
-            </div>
+            <PokemonCard
+              data={filteredData}
+              indexOfFirstCard={indexOfFirstCard}
+              indexOfLastCard={indexOfLastCard}
+            />
           )}
+
+          <Row className="pokemon-pagination">
+            <Col md={12}>
+              {filteredData && (
+                <Pagination
+                  onLeftClick={handlePrev}
+                  onRightClick={handleNext}
+                  currentPage={currentPage}
+                  totalPages={totalPages}
+                />
+              )}
+            </Col>
+          </Row>
         </div>
       </Row>
     </Container>
